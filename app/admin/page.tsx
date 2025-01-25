187ef95ed61a3c13c32f0c00617ea19ef1751a14
anthony vouin <avouin@normandiewebschool.fr>
2025-01-25 16:15:13 +0100

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -6,10 +6,6 @@ import QuestionList from '../components/admin/QuestionList'
 export default function AdminPage() {
   const router = useRouter()
 
-  const handleLogout = () => {
-    document.cookie = 'auth_token=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT'
-    router.push('/login')
-  }
 
   return (
     <div className="container mx-auto px-4 py-8">
